54f917f856a27f8ccacd656ee876db8291cc65ca
Christian Albrecht <christian.albrecht@knime.com>
2016-10-27 09:58:02 +0000

    IN-PROGRESS AP-6579: Define flow variables with type and default in Generic JavaScript View dialog https://knime-com.atlassian.net/browse/AP-6579
    Reducing variable assignment to simple value (instead of object with type and value). Adjusting node description.

--- org.knime.js.views/js-src/org/knime/js/base/node/viz/generic/knime-generic-view-v3.js
+++ org.knime.js.views/js-src/org/knime/js/base/node/viz/generic/knime-generic-view-v3.js
@@ -20,7 +20,7 @@ knime_generic_view = function() {
 			// Define settings object
 			SETTINGS = _value.settings ? JSON.parse(_value.settings) : {};
 			// Define variables object
-			FLOW_VARIABLES = _value.flowVariables || {}; 
+			extractFlowVariables();
 						
 			// Import style dependencies
 			var head = document.getElementsByTagName('head')[0];
@@ -68,20 +68,7 @@ knime_generic_view = function() {
 	};
 	
 	view.validate = function() {
-		var error;
-		if (FLOW_VARIABLES) {
-			for (key in FLOW_VARIABLES) {
-				var variable = FLOW_VARIABLES[key];
-				if (!variable || !variable.type) {
-					error = "Variable " + key + " was not correctly defined!";
-					break;
-				}
-				if (["INTEGER", "DOUBLE", "STRING"].indexOf(variable.type) < 0) {
-					error = "Variable " + key + " needs to be of a valid type, but was " + variable.type;
-					break;
-				}
-			}
-		}
+		var error = validateFlowVariables();
 		if (error) {
 			view.setValidationError(error);
 			return false;
@@ -97,11 +84,68 @@ knime_generic_view = function() {
 		if (!isObjectEmpty(SETTINGS)) {
 			_value.settings = JSON.stringify(SETTINGS);
 		}
-		if (!isObjectEmpty(FLOW_VARIABLES)) {
-			_value.flowVariables = FLOW_VARIABLES;
-		}
+		setFlowVariables();
 		return _value;
 	};
+	
+	extractFlowVariables = function() {
+		FLOW_VARIABLES = {};
+		if (_value && _value.flowVariables) {
+			for (var name in _value.flowVariables) {
+				var v = _value.flowVariables[name];
+				var val;
+				if (v.type == "INTEGER") {
+					val = v.intValue;
+				} else if (v.type == "DOUBLE") {
+					val = v.doubleValue;
+				} else {
+					val = v.stringValue;
+				}
+				FLOW_VARIABLES[name] = val;
+			}
+		}
+		return FLOW_VARIABLES;
+	}
+	
+	validateFlowVariables = function() {
+		if (_value && _value.flowVariables) {
+			for (var name in _value.flowVariables) {
+				var v = _value.flowVariables[name];
+				var newV = FLOW_VARIABLES[name];
+				if (typeof newV == 'undefined' || newV == null) {
+					return "The flow variable " + name + " must be defined.";
+				}
+				if (v.type == "INTEGER") {
+					if (newV != parseInt(newV, 10)) {
+						return "The flow variable value (" + newV + ") for " + name + " could not be parsed as an integer.";
+					}
+				} else  if (v.type == "DOUBLE") {
+					if (newV != parseFloat(newV)) {
+						return "The flow variable value (" + newV + ") for " + name + " could not be parsed as a double.";
+					}
+				}
+			}
+		}
+	}
+	
+	setFlowVariables = function() {
+		if (_value && _value.flowVariables) {
+			for (var name in _value.flowVariables) {
+				var v = _value.flowVariables[name];
+				var newV = FLOW_VARIABLES[name];
+				var val;
+				if (v.type == "INTEGER") {
+					v.intValue = parseInt(newV, 10);
+				} else if (v.type == "DOUBLE") {
+					v.doubleValue = parseFloat(newV);
+				} else {
+					v.stringValue = String(newV);
+				}
+			}
+		}
+	}
+	
+	
 	
 	isObjectEmpty = function(obj) {
 		return Object.keys(obj).length === 0 && obj.constructor === Object;
