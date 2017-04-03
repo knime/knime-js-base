37866e78798bfb29348a5c4cf140d06f5e34edfb
Oleg Yasnev <oleg.yasnev@knime.com>
2017-04-03 12:03:38 +0200

    AP-7004: Display missing values as red question marks in the view
    AP-7004 (Better display of missing values in JavaScript Table View)

--- org.knime.js.views/js-src/org/knime/js/base/node/viz/pagedTable/knime-paged-table.js
+++ org.knime.js.views/js-src/org/knime/js/base/node/viz/pagedTable/knime-paged-table.js
@@ -141,7 +141,8 @@ knime_paged_table = function() {
 				var colDef = {
 					'title': knimeTable.getColumnNames()[i],
 					'orderable' : isColumnSortable(colType),
-					'searchable': isColumnSearchable(colType)
+					'searchable': isColumnSearchable(colType),
+					'defaultContent': '<span class="missing-value-cell">?</span>'
 				}
 				if (knimeColType == 'Date and Time' && _representation.globalDateTimeFormat) {
 					colDef.render = function (data, type, full, meta) {
