5cfc3cbdeda918d3c966cbd558d8c8612c74a8a0
Oleg Yasnev <oleg.yasnev@knime.com>
2017-04-11 12:48:44 +0200

    AP-7189: Line Plot - Warning "Y axis columns with missing values are not be available"
    AP-7189 (Set warning message in JS scatter plot and line plot when
    missing values are omitted)

--- org.knime.js.views/src/org/knime/js/base/node/viz/plotter/line/LinePlotNodeModel.java
+++ org.knime.js.views/src/org/knime/js/base/node/viz/plotter/line/LinePlotNodeModel.java
@@ -357,6 +357,9 @@ final class LinePlotNodeModel extends AbstractSVGWizardNodeModel<LinePlotViewRep
         }
         if (m_config.getMissingValueMethod().equals(LinePlotViewConfig.MISSING_VALUE_METHOD_REMOVE_COLUMN) && missingValueColumns.length > 0) {
             setWarningMessage("Y axis columns with missing values will not be available in the view according to the chosen handling method.");
+            if (m_config.getShowWarningInView()) {
+                getViewRepresentation().setWarning("Y axis columns with missing values are not be available");
+            }
         }
 
         for (int col = 0; col < tableSpec.getNumColumns(); col++) {
