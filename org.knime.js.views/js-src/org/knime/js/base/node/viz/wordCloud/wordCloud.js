f0052c3a1925db697d1159a7bd163292fe8ed93e
Christian Albrecht <christian.albrecht@knime.com>
2017-10-03 14:21:18 -0700

    AP-5173: WIP on migrating to regular JS node
    AP-5173: JavaScript based Word Cloud

---
+++ org.knime.js.views/js-src/org/knime/js/base/node/viz/wordCloud/wordCloud.js
@@ -0,0 +1,131 @@
+wordcloud_ns = function() {
+
+	var wordCloud = {};
+	var _representation
+	var _value;
+
+	wordCloud.init = function(representation, value, layout_cloud) {
+		_representation = representation;
+		_value = value;
+		debugger;
+		var knimeTable = new kt();
+		knimeTable.setDataTable(_representation.inObjects[0]);
+		var fontSizeMinimum = _representation.options["fontSizeMinimum"];
+		var fontSizeMaximum = _representation.options["fontSizeMaximum"];
+		var wordCol = knimeTable
+				.getColumn(_representation.options["wordColumn"]);
+		if (typeof (wordCol) == "undefined") {
+			$("body")
+					.append(
+							'<div style="color: red">The selected word column does not exist</div>');
+		} else if (!_representation.options["useSizeProperty"]
+				&& typeof (knimeTable
+						.getColumn(_representation.options["sizeColumn"])) == "undefined") {
+			$("body")
+					.append(
+							'<div style="color: red">The selected size column does not exist</div>');
+		} else if (fontSizeMinimum > fontSizeMaximum) {
+			$("body")
+					.append(
+							'<div style="color: red">The font size minimum is bigger than the font size maximum</div>');
+		} else {
+			var font = _representation.options["font"];
+			var useSizeProperty = _representation.options["useSizeProperty"];
+			if (useSizeProperty) {
+				var sizeCol = knimeTable
+						.getColumn(_representation.options["sizeColumn"]);
+				// var sizeCol = knimeTable.getRowSizes();
+			} else {
+				var sizeCol = knimeTable
+						.getColumn(_representation.options["sizeColumn"]);
+			}
+			var sizeCol = normalizeArray(sizeCol, fontSizeMinimum,
+					fontSizeMaximum);
+			var colors = knimeTable.getRowColors();
+			var data = [];
+			for (var i = 0; i < wordCol.length; i++) {
+				var datum = {};
+				datum["text"] = wordCol[i];
+				datum["size"] = sizeCol[i];
+				datum["color"] = colors[i];
+				data.push(datum);
+			}
+			debugger;
+						var useColorProperty = _representation.options["useColorProperty"];
+						var fill = d3.scaleOrdinal(d3.schemeCategory20);
+						var colorPalette = _representation.options["colorPalette"];
+						switch (colorPalette) {
+						case "Palette-B":
+							fill = d3.scale.category20b();
+							break;
+						case "Palette-C":
+							fill = d3.scale.category20c();
+							break;
+						}
+						function draw(words) {
+							d3.select("body").append("svg").attr("width", 1000)
+									.attr("height", 1000).append("g").attr(
+											"transform", "translate(500,500)")
+									.selectAll("text").data(words).enter()
+									.append("text").style("font-size",
+											function(d) {
+												return d.size + "px";
+											}).style("font-family", font)
+									.style("fill", function(d, i) {
+										if (useColorProperty) {
+											return d.color;
+										} else {
+											return fill(i);
+										}
+									}).attr("text-anchor", "middle").attr(
+											"transform",
+											function(d) {
+												return "translate("
+														+ [ d.x, d.y ]
+														+ ")rotate(" + d.rotate
+														+ ")";
+											}).text(function(d) {
+										return d.text;
+									});
+						}
+						layout_cloud[0]().size([ 1000, 1000 ]).words(data)
+								.padding(5).rotate(function() {
+									return Math.random() * 120 - 60;
+								}).font(font).fontSize(function(d) {
+									return d.size;
+								}).on("end", draw).start();
+		}
+	}
+
+	normalizeArray = function(array, min, max) {
+		var newArray = [];
+		var range = max - min;
+		var arrayMin = Math.min.apply(null, array);
+		var arrayMax = Math.max.apply(null, array);
+		for (var i = 0; i < array.length; i++) {
+			newArray[i] = (array[i] - arrayMin) / (arrayMax - arrayMin);
+			newArray[i] = newArray[i] * range + min;
+		}
+		return newArray;
+	}
+
+	wordCloud.validate = function() {
+		return true;
+	}
+
+	wordCloud.setValidationError = function() {
+
+	}
+
+	wordCloud.getComponentValue = function() {
+		return _value;
+	}
+	
+	wordCloud.getSVG = function() {
+		var svg = d3.select("svg")[0][0];
+		return (new XMLSerializer()).serializeToString(svg);
+	}
+
+	return wordCloud;
+
+}();
---
+++ org.knime.js.views/src/org/knime/js/base/node/viz/wordCloud/WordCloudViewConfig.java
@@ -0,0 +1,125 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   2 Oct 2017 (albrecht): created
+ */
+package org.knime.js.base.node.viz.wordCloud;
+
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+
+/**
+ * Settings class holding the word cloud view config values
+ *
+ * @author Christian Albrecht, KNIME GmbH, Konstanz, Germany
+ */
+public class WordCloudViewConfig {
+
+    final static String CFG_HIDE_IN_WIZARD = "hideInWizard";
+    private final static boolean DEFAULT_HIDE_IN_WIZARD = false;
+    private boolean m_hideInWizard = DEFAULT_HIDE_IN_WIZARD;
+
+    final static String CFG_MAX_WORDS = "maxWords";
+    private final static int DEFAULT_MAX_WORDS = 250;
+    private int m_maxWords = DEFAULT_MAX_WORDS;
+
+    /**
+     * @return the hideInWizard
+     */
+    public boolean getHideInWizard() {
+        return m_hideInWizard;
+    }
+
+    /**
+     * @param hideInWizard the hideInWizard to set
+     */
+    public void setHideInWizard(final boolean hideInWizard) {
+        m_hideInWizard = hideInWizard;
+    }
+
+    /**
+     * @return the maxWords
+     */
+    public int getMaxWords() {
+        return m_maxWords;
+    }
+
+    /**
+     * @param maxWords the maxWords to set
+     */
+    public void setMaxWords(final int maxWords) {
+        m_maxWords = maxWords;
+    }
+
+    /** Saves current parameters to settings object.
+     * @param settings To save to.
+     */
+    public void saveSettings(final NodeSettingsWO settings) {
+        settings.addBoolean(CFG_HIDE_IN_WIZARD, m_hideInWizard);
+        settings.addInt(CFG_MAX_WORDS, m_maxWords);
+    }
+
+    /** Loads parameters in NodeModel.
+     * @param settings To load from.
+     * @throws InvalidSettingsException If incomplete or wrong.
+     */
+    public void loadSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_hideInWizard = settings.getBoolean(CFG_HIDE_IN_WIZARD);
+        m_maxWords = settings.getInt(CFG_MAX_WORDS);
+    }
+
+    /** Loads parameters in Dialog.
+     * @param settings To load from.
+     * @param spec The spec from the incoming data table
+     */
+    public void loadSettingsForDialog(final NodeSettingsRO settings, final DataTableSpec spec) {
+        m_hideInWizard = settings.getBoolean(CFG_HIDE_IN_WIZARD, DEFAULT_HIDE_IN_WIZARD);
+        m_maxWords = settings.getInt(CFG_MAX_WORDS, DEFAULT_MAX_WORDS);
+    }
+
+}
