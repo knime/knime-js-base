70db1c82da8675eef31599b82264a21f0d9fa9fa
Christian Albrecht <christian.albrecht@knime.com>
2017-10-20 17:02:12 -0700

    AP-5173: WIP
    AP-5173: JavaScript based Word Cloud

--- org.knime.js.views/js-src/org/knime/js/base/node/viz/wordCloud/wordCloud.js
+++ org.knime.js.views/js-src/org/knime/js/base/node/viz/wordCloud/wordCloud.js
@@ -5,15 +5,27 @@ knime_word_cloud = function() {
 	var _value;
 	var _sizeMin = Number.POSITIVE_INFINITY;
 	var _sizeMax = Number.NEGATIVE_INFINITY;
-	var _scale;
+	var _resizeTimeout;
 
 	wordCloud.init = function(representation, value) {
 		_representation = representation;
 		_value = value;
 		
-		if (_representation.imageGeneration && _value.svgFromView) {
-			// take last generated svg from view if available
+		if (!_representation.data) {
+			d3.select("body").text("Error: No data available");
 			return;
+		}
+		
+		d3.select("html").style("width", "100%").style("height", "100%")/*.style("overflow", "hidden")*/;
+		d3.select("body").style("width", "100%").style("height", "100%").style("margin", "0").style("padding", "0");
+		
+		if (_value.svgFromView) {
+			if (_representation.imageGeneration) {
+				return;
+			}
+			d3.select('body').html(_value.svgFromView);
+		} else {
+			d3.select('body').append('svg').append('g');
 		}
 		
 		_representation.data.sort(function(x, y) {
@@ -24,50 +36,82 @@ knime_word_cloud = function() {
 			_sizeMin = Math.min(_sizeMin, curSize);
 			_sizeMax = Math.max(_sizeMax, curSize);
 		}
+		
+		if (!_value.svgFromView) {
+			generateLayout();
+		}
+		drawControls();
+		if (!_representation.imageGeneration && _representation.resizeToWindow) {
+			var win = document.defaultView || document.parentWindow;
+			win.onresize = redraw;
+		}
+	}
+	
+	function getSize() {
+		if (_representation.imageGeneration || !_representation.resizeToWindow) {
+			return [_representation.imageWidth, _representation.imageHeight];
+		}
+		var pad = 20;
+		var doc = document.documentElement;
+		return [doc.clientWidth - pad, doc.clientHeight - pad /* minus titles */];
+	}
+	
+	function redraw() {
+		// debounce events
+		clearTimeout(_resizeTimeout);
+		_resizeTimeout = setTimeout(generateLayout, 100 /*ms*/);
+	}
+	
+	function getFontScale() {
+		var scale;
 		switch (_value.fontScaleType) {
 			case "logarithmic":
-				_scale = d3.scaleLog();
+				scale = d3.scaleLog();
 				break;
 			case "square root":
-				_scale = d3.scaleSqrt();
+				scale = d3.scaleSqrt();
 				break;
 			case "exponential":
-				_scale = d3.scalePow().exponent(2);
+				scale = d3.scalePow().exponent(2);
 				break;
 			default:
-				_scale = d3.scaleLinear();
+				scale = d3.scaleLinear();
 		}
-		_scale.domain([_sizeMin, _sizeMax]);
-		_scale.range([_value.minFontSize, _value.maxFontSize]);
-						
+		scale.domain([_sizeMin, _sizeMax]);
+		scale.range([_value.minFontSize, _value.maxFontSize]);
+		return scale;
+	}
+	
+	function generateLayout() {
+		var scale = getFontScale();
 		d3.layout.cloud()
-			.size([800, 600])
-			.words(_representation.data)
-			/*.padding(5)*/
-			.rotate(function() {
-				if (_value.numOrientations < 2) {
-					return _value.startAngle;
-				} else {
-					var range = (_value.endAngle - _value.startAngle) / (_value.numOrientations - 1);
-					return (~~(Math.random() * _value.numOrientations) * range) + _value.startAngle;
-				}
-			})
-			.font(_representation.font)
-			.fontSize(function(d) {
-				return _scale(d.size);
-			})
-			.timeInterval(10)
-			.spiral(_value.spiralType)
-			.on("end", draw).start();
-		drawControls();
+		.size(getSize())
+		.words(_representation.data)
+		/*.padding(5)*/
+		.rotate(function() {
+			if (_value.numOrientations < 2) {
+				return _value.startAngle;
+			} else {
+				var range = (_value.endAngle - _value.startAngle) / (_value.numOrientations - 1);
+				return (~~(Math.random() * _value.numOrientations) * range) + _value.startAngle;
+			}
+		})
+		.font(_representation.font)
+		.fontSize(function(d) {
+			return scale(d.size);
+		})
+		.timeInterval(10)
+		.spiral(_value.spiralType)
+		.on("end", draw).start();
 	}
 	
 	function draw(words, scale) {
-		d3.select("body").append("svg")
-			.attr("width", 800)
-			.attr("height", 600)
-			.append("g")
-			.attr("transform", "translate(400,300)")
+		var svg = d3.select("svg");
+		var size = getSize();
+		svg.attr("width", size[0])
+			.attr("height", size[1]);
+		svg.select("g")
+			.attr("transform", "translate(" + [size[0] >> 1, size[1] >> 1] + ")")
 			.selectAll("text").data(words).enter()
 			.append("text")
 			.style("font-size", function(d) {
@@ -87,8 +131,8 @@ knime_word_cloud = function() {
 			.text(function(d) {
 				return d.text;
 			});
-		var svg = d3.select("svg").node();
-		_value.svgFromView = (new XMLSerializer()).serializeToString(svg);
+		var svgNode = svg.node();
+		_value.svgFromView = (new XMLSerializer()).serializeToString(svgNode);
 	}
 	
 	function drawControls() {
@@ -123,14 +167,46 @@ knime_word_cloud = function() {
 	    	}
 	    	if (_representation.enableScaleTypeChange) {
 	    		var scaleOptions = ['linear', 'logarithmic', 'square root', 'exponential'];
-	    		var scaleSelect = knimeService.createMenuSelect('scaleTypeSelect', _value.fontScaleType, scaleOptions, function() {});
+	    		var scaleSelect = knimeService.createMenuSelect('scaleTypeSelect', _value.fontScaleType, scaleOptions, function() {
+	    			var scaleType = this.value;
+	    			if (_value.fontScaleType != scaleType) {
+	    				_value.fontScaleType = scaleType;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Font Scale:', 'expand', scaleSelect);
 	    	}
 	    	if (_representation.enableFontSizeChange) {
-	    		var minFontSizeField = knimeService.createMenuNumberField('minFontSizeField', _value.minFontSize, 1, null, 0.5, function() {});
+	    		var minFontSizeField = knimeService.createMenuNumberField('minFontSizeField', _value.minFontSize, 1, null, 0.5, function() {
+	    			var size = this.value;
+	    			if (size < 1) {
+	    				this.value = 1;
+	    				size = 1;
+	    			}
+	    			var maxSize = d3.select("#maxFontSizeField").node().value;
+	    			if (size > maxSize) {
+	    				this.value = maxSize;
+	    				size = maxSize;
+	    			}
+	    			if (_value.minFontSize != size) {
+	    				_value.minFontSize = size;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Minimum Font Size:', 'text-height', minFontSizeField, null, knimeService.SMALL_ICON);
 	    		
-	    		var maxFontSizeField = knimeService.createMenuNumberField('maxFontSizeField', _value.maxFontSize, 1, null, 0.5, function() {});
+	    		var maxFontSizeField = knimeService.createMenuNumberField('maxFontSizeField', _value.maxFontSize, 1, null, 0.5, function() {
+	    			var size = this.value;
+	    			var minSize = d3.select("#minFontSizeField").node().value;
+	    			if (size < minSize) {
+	    				this.value = minSize;
+	    				size = minSize;
+	    			}
+	    			if (_value.maxFontSize != size) {
+	    				_value.maxFontSize = size;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Maximum Font Size:', 'text-height', maxFontSizeField);
 	    	}
 	    	pre = true;
@@ -142,18 +218,64 @@ knime_word_cloud = function() {
 	    	}
 	    	if (_representation.enableSpiralTypeChange) {
 	    		var spiralOptions = ['archimedean', 'rectangular'];
-	    		var spiralSelect = knimeService.createMenuSelect('spiralSelect', _value.spiralType, spiralOptions, function() {});
+	    		var spiralSelect = knimeService.createMenuSelect('spiralSelect', _value.spiralType, spiralOptions, function() {
+	    			var type = this.value;
+	    			if (_value.spiralType != type) {
+	    				_value.spiralType = type;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Spiral Type:', 'repeat', spiralSelect);
 	    	}
 	    	if (_representation.enableNumOrientationsChange) {
-	    		var numOrientField = knimeService.createMenuNumberField('numOrientField', _value.numOrientations, 1, null, 1, function() {});
+	    		var numOrientField = knimeService.createMenuNumberField('numOrientField', _value.numOrientations, 1, null, 1, function() {
+	    			var num = this.value;
+	    			if (num < 1) {
+	    				this.value = 1;
+	    				num = 1;
+	    			}
+	    			if (_value.numOrientations != num) {
+	    				_value.numOrientations = num;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Number Orientations:', 'sort', numOrientField);
 	    	}
 	    	if (_representation.enableAnglesChange) {
-	    		var startAngleField = knimeService.createMenuNumberField('startAngleField', _value.startAngle, -90, 90, 1, function() {});
+	    		var startAngleField = knimeService.createMenuNumberField('startAngleField', _value.startAngle, -90, 90, 1, function() {
+	    			var angle = this.value;
+	    			if (angle < -90) {
+	    				this.value = -90;
+	    				angle = -90;
+	    			}
+	    			var endAngle = d3.select("#endAngleField").node().value;
+	    			if (angle > endAngle) {
+	    				this.value = endAngle;
+	    				angle = endAngle;
+	    			}
+	    			if (_value.startAngle != angle) {
+	    				_value.startAngle = angle;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('Start Angle:', 'chevron-left', startAngleField);
 	    		
-	    		var endAngleField = knimeService.createMenuNumberField('endAngleField', _value.endAngle, -90, 90, 1, function() {});
+	    		var endAngleField = knimeService.createMenuNumberField('endAngleField', _value.endAngle, -90, 90, 1, function() {
+	    			var angle = this.value;
+	    			if (angle > 90) {
+	    				this.value = 90;
+	    				angle = 90;
+	    			}
+	    			var startAngle = d3.select("#startAngleField").node().value;
+	    			if (angle < startAngle) {
+	    				this.value = startAngle;
+	    				angle = startAngle;
+	    			}
+	    			if (_value.endAngle != angle) {
+	    				_value.endAngle = angle;
+	    				redraw();
+	    			}
+	    		});
 	    		knimeService.addMenuItem('End Angle:', 'chevron-right', endAngleField);
 	    	}
 	    	pre = true;
