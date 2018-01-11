18165f9a7f628faacab4955bead7c4bfac1e3d3f
Christian Albrecht <christian.albrecht@knime.com>
2018-01-10 21:55:18 -0800

    AP-8099: hiding dialog option, removing it from node description
    AP-8099: Remove unsupported column types from Value Selection config dialog

--- org.knime.js.quickforms/src/org/knime/js/base/node/quickform/selection/value/ValueSelectionQuickFormNodeDialog.java
+++ org.knime.js.quickforms/src/org/knime/js/base/node/quickform/selection/value/ValueSelectionQuickFormNodeDialog.java
@@ -214,7 +214,8 @@ public class ValueSelectionQuickFormNodeDialog extends QuickFormNodeDialog {
     @Override
     protected final void fillPanel(final JPanel panelWithGBLayout, final GridBagConstraints gbc) {
         addPairToPanel("Selection Type: ", m_type, panelWithGBLayout, gbc);
-        addPairToPanel("Column Type: ", m_columnType, panelWithGBLayout, gbc);
+        //not adding to panel, until this option is actually needed/useful
+        //addPairToPanel("Column Type: ", m_columnType, panelWithGBLayout, gbc);
         addPairToPanel("Lock Column: ", m_lockColumn, panelWithGBLayout, gbc);
         addPairToPanel("Default Column: ", m_defaultColumnField, panelWithGBLayout, gbc);
         addPairToPanel("Default Value: ", m_defaultField, panelWithGBLayout, gbc);
