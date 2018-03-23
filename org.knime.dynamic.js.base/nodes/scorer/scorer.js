7c9124fb921c0647f73bc4e3adafb5b64490a434
Pascal Lee <pascal.lee@knime.com>
2018-03-23 09:25:05 +0100

    Adapted the Scorer Processor to changes in the Scorer Calculator. Redesigned the confusion matrix datatable in JS and added datable row selection related to a cell.

--- org.knime.dynamic.js.base/nodes/scorer/scorer.js
+++ org.knime.dynamic.js.base/nodes/scorer/scorer.js
@@ -1,10 +1,13 @@
 (scorer_namespace = function() {
 	
 	var scorer = {};
+	var classes;	//Classifications 
 	var confusionMatrix;
 	var keyStore;
 	var tableID;
+	var valueStatsList
 	var _representation, _value;
+	var confusionTable;
 	
 	scorer.init = function(representation, value) {
 		_representation = representation;
@@ -12,25 +15,65 @@
 		
 		debugger;
 		
-		var data = _representation.inObjects[1];
-		//keyStore = representation.inObjects[1];
-		tableID = representation.inObjects[0].id;
+		classes = _representation.inObjects[1].classes;
+		confusionMatrix = _representation.inObjects[1].confusionMatrix;
+		keyStore = representation.inObjects[1].keyStore;
+		tableID = _representation.inObjects[0].id;
+		valueStatsList = _representation.inObjects[1].valueStatsList;
 		
+
 		var body = document.querySelector('body');
+		
+		//Building the confusion matrix table
 		var table = document.createElement('table');
 		table.setAttribute('id', 'knime-confusion-matrix');
 		table.setAttribute('class', 'center');
-		var tBody = document.createElement('tbody');
-		table.appendChild(tBody);
 		
-		for (var row = 0; row <= data.length; row++) {
-			var tRow = document.createElement('tr');
-			for (var col = 0; col < data.length; col++) {
+		var tHeader = document.createElement('thead');
+		//1st header row
+		var tRow = document.createElement('tr');
+		//Total
+		var th = document.createElement('th');
+		th.appendChild(document.createTextNode('Total'));
+		th.setAttribute('colspan', 2);
+		th.setAttribute('rowspan', 2);
+		tRow.appendChild(th);
+		//Predicted
+		th = document.createElement('th');
+		th.appendChild(document.createTextNode('Predicted'));
+		th.setAttribute('colspan', classes.length);
+		tRow.appendChild(th);
+		tHeader.appendChild(tRow);
+		//2nd header row
+		tRow = document.createElement('tr');
+		for (var i = 0; i < classes.length; i++) {
+			th = document.createElement('th');
+			th.appendChild(document.createTextNode(classes[i]));
+			tRow.appendChild(th);
+		}
+		tHeader.appendChild(tRow);
+		table.appendChild(tHeader);
+		
+		var tBody = document.createElement('tbody');
+		for (var row = 0; row <= confusionMatrix.length; row++) {
+			tRow = document.createElement('tr');
+			if (row === 0) {
+				th = document.createElement('th');
+				th.appendChild(document.createTextNode('Actual'));
+				th.setAttribute('rowspan', classes.length);
+				tRow.appendChild(th);
+			}
+			if (row !== confusionMatrix.length) {
+				th = document.createElement('th');
+				th.appendChild(document.createTextNode(classes[row]));
+				tRow.appendChild(th);
+			}
+			for (var col = 0; col < confusionMatrix.length; col++) {
 				var tCell = document.createElement('td');
-				if (row === data.length) {
+				if (row === confusionMatrix.length) {
 					tCell.setAttribute('class', 'no-border');
 				} else { 
-					tCell.appendChild(document.createTextNode(data[row][col]));
+					tCell.appendChild(document.createTextNode(confusionMatrix[row][col]));
 					tCell.setAttribute('data-row', row);
 					tCell.setAttribute('data-col', col);
 					if (row === col) {
@@ -45,21 +88,46 @@
 			tRow.appendChild(emptyCell);
 			tBody.appendChild(tRow);
 		}
+		table.appendChild(tBody);
+		
+		confusionTable = table;
 		body.appendChild(table);
-		confusionMatrix = table;
+
+
+		// //Building the accuracy statistics table
+		// table = document.createElement('table');
+		// table.setAttribute('id', 'knime-accuracy-statistics');
+		// table.setAttribute('class', 'center');
+
+		// tHeader = document.createElement('thead');
+		// tRow = document.createElement('tr');
+		// th = document.createElement('th');
+		// th.appendChild(document.createTextNode('Class'));
+		// tRow.appendChild(th);
+		// // for (var i = 0; i < classes.length; i++) {
+		// // 	th = document.createElement('th');
+		// // 	th.appendChild(document.createTextNode(classes[i]));
+		// // 	tRow.appendChild(th);
+		// // }
+
+
+		// table.appendChild(tHeader);
+
+		// body.appendChild(table);
+		
 		
 		knimeService.subscribeToSelection(tableID, selectionChanged);
 	}
 	
 	cellClicked = function(event) {
-		confusionMatrix.querySelectorAll('td').forEach(function (cell) {
+		confusionTable.querySelectorAll('td').forEach(function (cell) {
 			cell.classList.remove('selected');
 		});
 		this.classList.add('selected');
 		if (knimeService.isInteractivityAvailable()) {
-			knimeService.setSelectedRows(tableID, ['Row20'], selectionChanged);
-			/*var rowIds = keyStore[this.dataset.row][this.dataset.col];
-			knimeService.setSelectedRows(tableID, rowIds);*/
+			// knimeService.setSelectedRows(tableID, ['Row20'], selectionChanged);
+			var rowIds = keyStore[this.dataset.row][this.dataset.col];
+			knimeService.setSelectedRows(tableID, rowIds);
 		}
 	}
 	
--- org.knime.dynamic.js.base/src/org/knime/dynamic/js/base/scorer/ScorerProcessor.java
+++ org.knime.dynamic.js.base/src/org/knime/dynamic/js/base/scorer/ScorerProcessor.java
@@ -1,6 +1,11 @@
 package org.knime.dynamic.js.base.scorer;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.knime.base.node.mine.scorer.accuracy.AccuracyScorerCalculator;
+import org.knime.base.node.mine.scorer.accuracy.AccuracyScorerCalculator.ValueStats;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
@@ -15,25 +20,105 @@ public class ScorerProcessor extends DynamicStatefulJSProcessor {
 			throws Exception {
         BufferedDataTable table = (BufferedDataTable)inObjects[0];
         
-//        String[] columnNames = ((SettingsModelColumnFilter2)config.getModel("columns")).applyTo(table.getDataTableSpec()).getIncludes();
-//        if (columnNames.length != 2) {
-//            throw new InvalidSettingsException("Please choose two distincts columns.");
-//        }
-        
         SettingsModelString firstColumnModel  = (SettingsModelString) config.getModel("first_column");
         SettingsModelString secondColumnModel  = (SettingsModelString) config.getModel("second_column");
-        
         String firstColumnName = firstColumnModel.getStringValue();
         String secondColumnName = secondColumnModel.getStringValue();
-        
-        
+                
         AccuracyScorerCalculator scorerCalc = new AccuracyScorerCalculator(table, firstColumnName, secondColumnName, exec);
+        String[] classes = scorerCalc.getTargetValues();
+        List<String>[][] keyStore = scorerCalc.getKeyStore();
         int[][] confusionMatrix = scorerCalc.getConfusionMatrix();
+        List<ValueStats> valueStatsList = new ArrayList<ValueStats>();
+        Iterator<ValueStats> valueStatsIterator = scorerCalc.getIterator();
+        while(valueStatsIterator.hasNext()) {
+        	valueStatsList.add(valueStatsIterator.next());
+        }
+        double accuracy = scorerCalc.getAccuracy();
+        double cohensKappa = scorerCalc.getCohensKappa();
         
         //TODO: make this into a proper JSONDataTable, see DataExplorer for example
+//        return new Object[]{table, confusionMatrix, valueStatsList};
+        ScorerResult result = new ScorerResult(classes, confusionMatrix, keyStore, valueStatsList, accuracy, cohensKappa);
         
-
-        return new Object[]{table, confusionMatrix};
+        return new Object[] {table, result};
 	}
 
+	//Class for wrapping all the results of the ScorerCalculator into one unique structure
+	public class ScorerResult {
+		private String[] classes;
+		private int[][] confusionMatrix;
+		private List<String>[][] keyStore;
+		private List<ValueStats> valueStatsList;
+	    private double accuracy;
+	    private double cohensKappa;
+	    
+		public String[] getClasses() {
+			return classes;
+		}
+
+		public void setClasses(String[] classes) {
+			this.classes = classes;
+		}
+
+		public int[][] getConfusionMatrix() {
+			return confusionMatrix;
+		}
+		
+		public void setConfusionMatrix(int[][] confusionMatrix) {
+			this.confusionMatrix = confusionMatrix;
+		}
+		
+		public List<String>[][] getKeyStore() {
+			return keyStore;
+		}
+
+		public void setKeyStore(List<String>[][] keyStore) {
+			this.keyStore = keyStore;
+		}
+
+		public List<ValueStats> getValueStatsList() {
+			return valueStatsList;
+		}
+		
+		public void setValueStatsList(List<ValueStats> valueStatsList) {
+			this.valueStatsList = valueStatsList;
+		}
+
+		public double getAccuracy() {
+			return accuracy;
+		}
+
+		public void setAccuracy(double accuracy) {
+			this.accuracy = accuracy;
+		}
+
+		public double getCohensKappa() {
+			return cohensKappa;
+		}
+
+		public void setCohensKappa(double cohensKappa) {
+			this.cohensKappa = cohensKappa;
+		}
+	    
+		public ScorerResult() {
+		}
+
+		/**
+		 * @param confusionMatrix
+		 * @param valueStatsList
+		 * @param m_accuracy
+		 * @param m_cohensKappa
+		 */
+		public ScorerResult(String[] classes, int[][] confusionMatrix, List<String>[][] keyStore, List<ValueStats> valueStatsList, double accuracy,
+				double cohensKappa) {
+			super();
+			this.classes = classes;
+			this.confusionMatrix = confusionMatrix;
+			this.keyStore = keyStore;
+			this.valueStatsList = valueStatsList;
+			this.accuracy = accuracy;
+			this.cohensKappa = cohensKappa;
+		}
+	}	
 }
