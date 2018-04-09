dfe5552223bf4d3d417b027dd8d910d0a416ab73
Pascal Lee <pascal.lee@knime.com>
2018-04-09 11:42:44 +0200

    The rows and columns rates on the confusion matrix can now be toggled visible or hidden. The header color in the dialog option is now applied to all the headers of the view.

--- org.knime.dynamic.js.base/nodes/scorer/scorer.js
+++ org.knime.dynamic.js.base/nodes/scorer/scorer.js
@@ -17,18 +17,19 @@
 		
 		debugger;
 		
-		drawPage();
+		createPage();
 
 		if (_representation.options.enableViewControls) {
 			drawControls();
 		}
 	}
 
-	function drawPage() {
+	function createPage() {
 		title = _value.options["title"];
 		subtitle = _value.options["subtitle"];
 		classes = _representation.inObjects[0].classes;
 		confusionMatrix = _representation.inObjects[0].confusionMatrix;
+		confusionMatrixWithRates = _representation.inObjects[0].confusionMatrixWithRates;
 		keyStore = _representation.inObjects[0].keyStore;
 		tableID = _representation.tableIds[0];
 		valueStatsList = _representation.inObjects[0].valueStatsList;
@@ -67,12 +68,14 @@
 		th.appendChild(document.createTextNode('Total rows number: \n' + rowsNumber));
 		th.setAttribute('colspan', 2);
 		th.setAttribute('rowspan', 2);
-		th.setAttribute('style', 'border-right-width: 2px')
+		th.setAttribute('style', 'border-right-width: 2px');
+		th.style.backgroundColor = _representation.options.header_color;
 		tRow.appendChild(th);
 		//Predicted
 		th = document.createElement('th');
 		th.appendChild(document.createTextNode('Predicted'));
 		th.setAttribute('colspan', classes.length);
+		th.style.backgroundColor = _representation.options.header_color;
 		tRow.appendChild(th);
 		tHeader.appendChild(tRow);
 		//2nd header row
@@ -80,46 +83,60 @@
 		for (var i = 0; i < classes.length; i++) {
 			th = document.createElement('th');
 			th.appendChild(document.createTextNode(classes[i]));
+			th.style.backgroundColor = _representation.options.header_color;
 			tRow.appendChild(th);
 		}
 		tHeader.appendChild(tRow);
 		table.appendChild(tHeader);
 		
 		var tBody = document.createElement('tbody');
-		for (var row = 0; row <= confusionMatrix.length; row++) {
+		for (var row = 0; row < confusionMatrix.length; row++) {
 			tRow = document.createElement('tr');
 			if (row === 0) {
 				th = document.createElement('th');
 				th.appendChild(document.createTextNode('Actual'));
 				th.setAttribute('rowspan', classes.length);
-				th.setAttribute('style', 'border-bottom-width: 2px')
+				th.setAttribute('style', 'border-bottom-width: 2px');
+				th.style.backgroundColor = _representation.options.header_color;
 				tRow.appendChild(th);
 			}
-			if (row !== confusionMatrix.length) {
-				th = document.createElement('th');
-				th.appendChild(document.createTextNode(classes[row]));
-				tRow.appendChild(th);
-			}
+			th = document.createElement('th');
+			th.appendChild(document.createTextNode(classes[row]));
+			th.style.backgroundColor = _representation.options.header_color;
+			tRow.appendChild(th);
 			for (var col = 0; col < confusionMatrix.length; col++) {
 				var tCell = document.createElement('td');
-				if (row === confusionMatrix.length) {
-					tCell.setAttribute('class', 'no-border');
-				} else { 
-					tCell.appendChild(document.createTextNode(confusionMatrix[row][col]));
-					tCell.setAttribute('data-row', row);
-					tCell.setAttribute('data-col', col);
-					if (row === col) {
-						tCell.style.backgroundColor = _representation.options.diag_color;
-					}
-					tCell.onclick = cellClicked;
+				tCell.appendChild(document.createTextNode(confusionMatrix[row][col]));
+				tCell.setAttribute('data-row', row);
+				tCell.setAttribute('data-col', col);
+				if (row === col) {
+					tCell.style.backgroundColor = _representation.options.diag_color;
 				}
+				tCell.onclick = cellClicked;
 				tRow.appendChild(tCell);
 			}
-			var emptyCell = document.createElement('td');
-			emptyCell.setAttribute('class', 'no-border');
-			tRow.appendChild(emptyCell);
+			var lastCell = document.createElement('td');
+			lastCell.appendChild(document.createTextNode(confusionMatrixWithRates[row][confusionMatrixWithRates.length-1].toFixed(3)));
+			lastCell.setAttribute('class', 'rateCell');
+			tRow.appendChild(lastCell);
 			tBody.appendChild(tRow);
 		}
+		tRow = document.createElement('tr');
+		var td = document.createElement('td');
+		td.setAttribute('class', 'no-border');
+		tRow.appendChild(td);
+		td = document.createElement('td');
+		td.setAttribute('class', 'no-border');
+		tRow.appendChild(td);
+		for (var col = 0; col < confusionMatrix.length; col++) {
+			td = document.createElement('td');
+			td.appendChild(document.createTextNode(confusionMatrixWithRates[confusionMatrixWithRates.length-1][col].toFixed(3)));
+			td.setAttribute('class', 'rateCell');			
+			tRow.appendChild(td);
+		}
+
+
+		tBody.appendChild(tRow);
 		table.appendChild(tBody);
 		
 		confusionTable = table;
@@ -142,6 +159,7 @@
 		for (var i = 0; i < statNames.length; i++) {
 			th = document.createElement('th');
 			th.appendChild(document.createTextNode(statNames[i]));
+			th.style.backgroundColor = _representation.options.header_color;
 			tRow.appendChild(th);
 		}
 		tHeader.appendChild(tRow);
@@ -154,6 +172,7 @@
 			var th = document.createElement('th');
 			if (i !== valueStatsList.length) {
 				th.appendChild(document.createTextNode(valueStatsList[i].valueName));
+				th.style.backgroundColor = _representation.options.header_color;
 			} else {
 				th = document.createElement('td');
 				th.setAttribute('class', 'no-border');
@@ -257,6 +276,7 @@
 		tRow = document.createElement('tr');
 		th = document.createElement('th');
 		th.appendChild(document.createTextNode('Overall Accuracy'));
+		th.style.backgroundColor = _representation.options.header_color;
 		tRow.appendChild(th);
 		td = document.createElement('td');
 		td.appendChild(document.createTextNode(accuracy.toFixed(3)));
@@ -266,6 +286,7 @@
 		tRow = document.createElement('tr');
 		th = document.createElement('th');
 		th.appendChild(document.createTextNode("Cohen's kappa"));
+		th.style.backgroundColor = _representation.options.header_color;
 		tRow.appendChild(th);
 		td = document.createElement('td');
 		td.appendChild(document.createTextNode(cohensKappa.toFixed(3)));
@@ -279,6 +300,9 @@
 		knimeService.subscribeToSelection(tableID, selectionChanged);
 	}
 
+	createConfusionMatrixTable = function() {
+
+	}
 	
 	cellClicked = function(event) {
 		confusionTable.querySelectorAll('td').forEach(function (cell) {
@@ -341,7 +365,7 @@
 	    	var switchCMRatesDisplay = knimeService.createMenuCheckbox('switchCMRatesDisplay', _value.options.displayCMRates, function() {
 	    		if (_value.options.displayCMRates != this.checked) {
 					_value.options.displayCMRates = this.checked;
-					// toggleClassStatisticsDisplay();
+					toggleConfusionMatrixRatesDisplay();
 				}
 	    	});
 	    	knimeService.addMenuItem("Display confusion matrix rates: ", 'table', switchCMRatesDisplay);
@@ -397,6 +421,16 @@
 			d3.select("#knime-class-statistics").style("display", "none");
 		}
 	}
+
+	function toggleConfusionMatrixRatesDisplay() {
+		if (_value.options.displayCMRates === true) {
+			d3.selectAll(".rateCell").style("visibility", "visible");
+
+		} else {
+			d3.selectAll(".rateCell").style("visibility", "hidden");
+			d3.selectAll(".no-border").style("visibility", "hidden");
+		}
+	}	
 
 	scorer.validate = function() {
 		return true;
