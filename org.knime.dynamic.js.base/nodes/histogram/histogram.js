e502fbbbb681c42eda113551e489d94c52929360
Christian Albrecht <christian.albrecht@knime.com>
2018-11-27 19:03:11 +0100

    AP-7812: rename occurrence count to binned column name
    AP-7812 (Javascript Histogram)

--- org.knime.dynamic.js.base/nodes/histogram/histogram.js
+++ org.knime.dynamic.js.base/nodes/histogram/histogram.js
@@ -8,9 +8,14 @@
 		_representation = representation;
 		var binningResult = _representation.inObjects[0];
 		var binColName = binningResult.binnedColumn;
+		var orgColName = _representation.options['cat'];
 		_representation.inObjects[0] = binningResult.groups;
 		_representation.options['cat'] = binColName;
 		_representation.isHistogram = true;
+		var optMethod = _representation.options['aggr'];
+		if (optMethod == 'Occurence\u00A0Count') {
+            _representation.inObjects[0].table.spec.colNames[1] = orgColName; 
+        }
 		grouped_bar_chart_namespace.init(_representation, _value);
 	}
 
