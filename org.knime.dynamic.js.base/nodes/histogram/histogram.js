5e4d1abf8e5c6eadbd4fb065cb46a124873bf0b1
Christian Albrecht <christian.albrecht@knime.com>
2019-01-24 12:57:01 +0100

    AP-11267: Lint histogram, use new bar chart namespace
    AP-11267 (Lint histogram)

--- org.knime.dynamic.js.base/nodes/histogram/histogram.js
+++ org.knime.dynamic.js.base/nodes/histogram/histogram.js
@@ -1,36 +1,37 @@
-(histogram_namespace = function() {
+/* global knimeGroupedBarChart:false */
+window.knimeHistogram = (function () {
 
-	var histogram = {};
-	var _representation, _value;
+    var histogram = {};
+    var _representation, _value;
 
-	histogram.init = function(representation, value) {
-		_value = value;
-		_representation = representation;
-		var binningResult = _representation.inObjects[0];
-		var binColName = binningResult.binnedColumn;
-		var orgColName = _representation.options['cat'];
-		_representation.inObjects[0] = binningResult.groups;
-		_representation.options['cat'] = binColName;
-		_representation.isHistogram = true;
-		var optMethod = _representation.options['aggr'];
-		if (optMethod == 'Occurence\u00A0Count') {
-            _representation.inObjects[0].table.spec.colNames[1] = orgColName; 
+    histogram.init = function (representation, value) {
+        _value = value;
+        _representation = representation;
+        var binningResult = _representation.inObjects[0];
+        var binColName = binningResult.binnedColumn;
+        var orgColName = _representation.options.cat;
+        _representation.inObjects[0] = binningResult.groups;
+        _representation.options.cat = binColName;
+        _representation.isHistogram = true;
+        var optMethod = _representation.options.aggr;
+        if (optMethod === 'Occurence\u00A0Count') {
+            _representation.inObjects[0].table.spec.colNames[1] = orgColName;
         }
-		grouped_bar_chart_namespace.init(_representation, _value);
-	}
+        knimeGroupedBarChart.init(_representation, _value);
+    };
 
-	histogram.validate = function() {
-		return grouped_bar_chart_namespace.validate();
-	};
+    histogram.validate = function () {
+        return knimeGroupedBarChart.validate();
+    };
 
-	histogram.getComponentValue = function() {
-		return grouped_bar_chart_namespace.getComponentValue();
-	};
+    histogram.getComponentValue = function () {
+        return knimeGroupedBarChart.getComponentValue();
+    };
 
-	histogram.getSVG = function() {
-		return grouped_bar_chart_namespace.getSVG();
-	}
+    histogram.getSVG = function () {
+        return knimeGroupedBarChart.getSVG();
+    };
 
-	return histogram;
+    return histogram;
 
-}());
+})();
