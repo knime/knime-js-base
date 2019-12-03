a81a10b7b78578eb5065944a31e4e4896a2641ac
Thomas Rosenau <thomas.rosenau@knime.com>
2019-12-03 11:14:41 +0100

    Fix lints / formattting

--- org.knime.js.testing/js-src/org/knime/js/node/minimalRequestHandler/requestHandlerLazyLoad.js
+++ org.knime.js.testing/js-src/org/knime/js/node/minimalRequestHandler/requestHandlerLazyLoad.js
@@ -1,26 +1,34 @@
-if (requestHandler) {
-	requestHandler.initRequest = function(request, textArea, representation, curRequests) {
-		
-		/* this is where the magic happens */
-		var promise = knimeService.requestViewUpdate(request, representation.keepOrder);
-		promise.progress(monitor => requestHandler.displayProgress(monitor))
-			.then(response => requestHandler.displayResponse(response))
-			.catch(error => requestHandler.displayError(request.sequence, error));
-		/* end magic */
-		
-		if (promise.monitor && promise.monitor.requestSequence) {
-			curRequests.push(promise);
-			var text = "Issued request sequences: [";
-			for (var i = 0; i < curRequests.length; i++) {
-				text += curRequests[i].monitor.requestSequence;
-				if (i < curRequests.length - 1) {
-					text += ", ";
-				}
-			}
-			text += "]\n";
-			textArea.value += text;
-			textArea.scrollTop = textArea.scrollHeight;
-		}
-		
-	}
+if (window.requestHandler) {
+    /* global requestHandler:false */
+    requestHandler.initRequest = function (request, textArea, representation, curRequests) {
+
+        /* this is where the magic happens */
+        var promise = knimeService.requestViewUpdate(request, representation.keepOrder);
+        promise
+            .progress(function (monitor) {
+                return requestHandler.displayProgress(monitor);
+            })
+            .then(function (response) {
+                return requestHandler.displayResponse(response);
+            })
+            .catch(function (error) {
+                return requestHandler.displayError(request.sequence, error);
+            });
+        /* end magic */
+
+        if (promise.monitor && promise.monitor.requestSequence) {
+            curRequests.push(promise);
+            var text = 'Issued request sequences: [';
+            for (var i = 0; i < curRequests.length; i++) {
+                text += curRequests[i].monitor.requestSequence;
+                if (i < curRequests.length - 1) {
+                    text += ', ';
+                }
+            }
+            text += ']\n';
+            textArea.value += text;
+            textArea.scrollTop = textArea.scrollHeight;
+        }
+
+    };
 }
