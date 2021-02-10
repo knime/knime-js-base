3b1002d1e6d0a761e4c9fe2be78cdd6c76d6ccf4
Ben Laney <ben.laney@knime.com>
2021-02-10 16:24:01 +0100

    AP-15952: Add Refresh Button Widget
    AP-15952 ('refresh' button widget)

---
+++ org.knime.js.quickforms/src/org/knime/js/base/node/widget/reactive/refresh/RefreshButtonWidgetNodeConfig.java
@@ -0,0 +1,208 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.js.base.node.widget.reactive.refresh;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.js.base.util.LabeledViewConfig;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The node configuration for the refresh button widget node.
+ *
+ * @author Ben Laney, KNIME GmbH, Konstanz, Germany
+ */
+public class RefreshButtonWidgetNodeConfig extends LabeledViewConfig {
+
+    protected static final String FLOW_VARIABLE_NAME = "refresh_widget";
+
+    private static final String DEFAULT_LABEL = "";
+    private static final String CFG_LABEL = "label";
+    private String m_label = DEFAULT_LABEL;
+
+    private static final String DEFAULT_DESCRIPTION = "";
+    private static final String CFG_DESCRIPTION = "description";
+    private String m_description = DEFAULT_DESCRIPTION;
+
+    private static final String DEFAULT_TEXT = "Refresh";
+    private static final String CFG_TEXT = "text";
+    private String m_text = DEFAULT_TEXT;
+
+    /**
+     * @return the label
+     */
+    @Override
+    @JsonProperty("label")
+    public String getLabel() {
+        return m_label;
+    }
+
+    /**
+     * @param label the label to set
+     */
+    @Override
+    public void setLabel(final String label) {
+        m_label = label;
+    }
+
+    /**
+     * @return the description
+     */
+    @Override
+    @JsonProperty("description")
+    public String getDescription() {
+        return m_description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    @Override
+    public void setDescription(final String description) {
+        m_description = description;
+    }
+
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return m_text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(final String text) {
+        m_text = text;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void saveSettings(final NodeSettingsWO settings) {
+        settings.addString(CFG_LABEL, m_label);
+        settings.addString(CFG_DESCRIPTION, m_description);
+        settings.addString(CFG_TEXT, m_text);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void loadSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_text = settings.getString(CFG_TEXT, DEFAULT_TEXT);
+        m_label = settings.getString(CFG_LABEL, DEFAULT_LABEL);
+        m_description = settings.getString(CFG_DESCRIPTION, DEFAULT_DESCRIPTION);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void loadSettingsInDialog(final NodeSettingsRO settings) {
+        super.loadSettingsInDialog(settings);
+        try {
+            loadSettings(settings);
+        } catch (InvalidSettingsException e) {
+            // do nothing
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(RefreshButtonWidgetViewRepresentation.class);
+        sb.append(", label=");
+        sb.append(m_label);
+        sb.append(", description=");
+        sb.append(m_description);
+        sb.append(", format=");
+        return sb.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(m_label)
+            .append(m_description)
+            .append(m_text)
+            .toHashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        RefreshButtonWidgetNodeConfig other = (RefreshButtonWidgetNodeConfig)obj;
+        return new EqualsBuilder()
+            .append(m_label, other.m_label)
+            .append(m_description, other.m_description)
+            .append(m_text, other.m_text)
+            .isEquals();
+    }
+}
---
+++ org.knime.js.quickforms/src/org/knime/js/base/node/widget/reactive/refresh/RefreshButtonWidgetNodeModel.java
@@ -0,0 +1,211 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.js.base.node.widget.reactive.refresh;
+
+import java.util.NoSuchElementException;
+
+import org.knime.base.util.flowvariable.FlowVariableProvider;
+import org.knime.base.util.flowvariable.FlowVariableResolver;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObject;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObjectSpec;
+import org.knime.core.node.web.ValidationError;
+import org.knime.js.core.node.AbstractWizardNodeModel;
+
+/**
+ * The node model for the refresh button node.
+ *
+ * @author Ben Laney, KNIME GmbH, Konstanz, Germany
+ */
+public class RefreshButtonWidgetNodeModel extends AbstractWizardNodeModel<RefreshButtonWidgetViewRepresentation,
+    RefreshButtonWidgetViewValue> implements FlowVariableProvider {
+
+    private RefreshButtonWidgetNodeConfig m_config = new RefreshButtonWidgetNodeConfig();
+
+    /**
+     * Creates a new refresh button widget node model.
+     * @param viewName the view name
+     */
+    public RefreshButtonWidgetNodeModel(final String viewName) {
+        super(new PortType[0], new PortType[]{FlowVariablePortObject.TYPE}, viewName);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        return new PortObjectSpec[]{FlowVariablePortObjectSpec.INSTANCE};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObject[] performExecute(final PortObject[] inObjects, final ExecutionContext exec)
+        throws Exception {
+        synchronized (getLock()) {
+            RefreshButtonWidgetViewRepresentation representation = getViewRepresentation();
+            if (representation == null) {
+                representation = createEmptyViewRepresentation();
+            }
+            representation.setLabel(m_config.getLabel());
+            representation.setDescription(m_config.getDescription());
+            String flowVarCorrectedText;
+            try {
+                flowVarCorrectedText = FlowVariableResolver.parse(m_config.getText(), this);
+            } catch (NoSuchElementException nse) {
+                throw new InvalidSettingsException(nse.getMessage(), nse);
+            }
+            representation.setText(flowVarCorrectedText);
+            pushFlowVariableString(RefreshButtonWidgetNodeConfig.FLOW_VARIABLE_NAME, flowVarCorrectedText);
+        }
+        return new PortObject[]{FlowVariablePortObject.INSTANCE};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ValidationError validateViewValue(final RefreshButtonWidgetViewValue viewContent) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RefreshButtonWidgetViewRepresentation createEmptyViewRepresentation() {
+        return new RefreshButtonWidgetViewRepresentation();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RefreshButtonWidgetViewValue createEmptyViewValue() {
+        return new RefreshButtonWidgetViewValue();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getJavascriptObjectID() {
+        return "org.knime.js.base.node.widget.reactive.refresh";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isHideInWizard() {
+        return m_config.getHideInWizard();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setHideInWizard(final boolean hide) {
+        m_config.setHideInWizard(hide);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_config.saveSettings(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+        new RefreshButtonWidgetNodeConfig().loadSettings(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+        m_config.loadSettings(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void performReset() {
+        // do nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void saveCurrentValue(final NodeSettingsWO content) {
+        // do nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void useCurrentValueAsDefault() {
+        // do nothing
+    }
+}
