a98baf7ae3aadce179c41c50b89ddc3521fd09db
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2021-11-30 08:43:22 +0100

    AP-17717: Added Objects.equals to prevent NPE
    AP-17717 (Credentials Widget does not work with the refresh button without a default value set)

--- org.knime.js.quickforms/src/org/knime/js/base/node/widget/input/credentials/CredentialsWidgetNodeModel.java
+++ org.knime.js.quickforms/src/org/knime/js/base/node/widget/input/credentials/CredentialsWidgetNodeModel.java
@@ -51,6 +51,7 @@ package org.knime.js.base.node.widget.input.credentials;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.lang.StringUtils;
@@ -147,7 +148,7 @@ public class CredentialsWidgetNodeModel extends
 
         var defaultVal = config.getDefaultValue();
         var previousDefaultVal = previousConfig.getDefaultValue();
-        if (!previousDefaultVal.getPassword().equals(defaultVal.getPassword())) {
+        if (!Objects.equals(previousDefaultVal.getPassword(), defaultVal.getPassword())) {
             currentViewValue.setPassword(defaultVal.getPassword());
         }
         if (!previousDefaultVal.getUsername().equals(defaultVal.getUsername())) {
