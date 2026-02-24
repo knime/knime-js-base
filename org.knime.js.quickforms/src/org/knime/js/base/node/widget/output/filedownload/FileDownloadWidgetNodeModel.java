5f772d99881db271e0a48d9ad8977f5946a388b6
Robin Gerling <75605520+robin-gerling@users.noreply.github.com>
2026-02-24 08:55:22 +0100

    UIEXT-3353: Remove logging from external output generation
    ...because an exception will be thrown in configure or execute
    
    UIEXT-3353 (File Download Widget Cloggs Logs)

--- org.knime.js.quickforms/src/org/knime/js/base/node/widget/output/filedownload/FileDownloadWidgetNodeModel.java
+++ org.knime.js.quickforms/src/org/knime/js/base/node/widget/output/filedownload/FileDownloadWidgetNodeModel.java
@@ -317,13 +317,11 @@ public class FileDownloadWidgetNodeModel extends AbstractWizardNodeModel<FileDow
         try {
             URI uri = getPathFromVariable().toUri();
             data.resource(uri);
-        } catch (InvalidSettingsException ex) {
-            getLogger().error("Could not get output resource URL: " + ex.getMessage(), ex);
-            try {
-                data.resource(new URI("unknown-filename"));
-            } catch (URISyntaxException e1) {
-                getLogger().error("Error while creating resource URI for unknown file: " + e1.getMessage(), e1);
-            }
+        } catch (InvalidSettingsException ex) { // NOSONAR
+            /* The variable/path is already validated in configure/execute (see usages of getPathFromVariable). If it
+             * fails here the node itself is in an idle state and the fallback without explicit log entry is desirable.
+             */
+            data.resource(URI.create("unknown-filename"));
         }
 
         return data.build();
